adb349c95c74a6f801b8b1950cb699e6fb86562d
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-11-26 13:18:55 +0530

    Add till async methods, before http module

---
+++ NodeFCC/10_fs_sync.js
@@ -0,0 +1,14 @@
+const {readFileSync, writeFileSync} = require('fs')
+console.log('Start')
+const first = readFileSync('./content/first.txt','utf8');
+const second = readFileSync('./content/second.txt','utf8');
+
+console.log(first,second);
+writeFileSync(
+    './content/result.txt',
+    `Here is the result : ${first},${second}`,
+    {flag: 'a'}    
+)
+
+console.log('Done with this task')
+console.log('Starting next task')
