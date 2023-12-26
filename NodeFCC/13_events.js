7291659a9c293e13c145dcfd750ceddd7b9460dd
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-26 20:03:13 +0530

    Add final code from original repo

--- NodeFCC/13_events.js
+++ NodeFCC/13_events.js
@@ -23,3 +23,26 @@ customEmitter.on('response',() =>{
 customEmitter.emit('response', 'john', 34);
 //order matters, if .emit was before all the .on then we won't get any output
 
+
+/*
+Final code
+const EventEmitter = require('events')
+
+const customEmitter = new EventEmitter()
+
+// on and emit methods
+// keep track of the order
+// additional arguments
+// built-in modules utilize it
+
+customEmitter.on('response', (name, id) => {
+  console.log(`data recieved user ${name} with id:${id}`)
+})
+
+customEmitter.on('response', () => {
+  console.log('some other logic here')
+})
+
+customEmitter.emit('response', 'john', 34)
+
+*/
