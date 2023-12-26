01613669197b6a9b4a218deddd38c457384c480d
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-26 13:41:41 +0530

    add comment on different environmental variables usage

--- NodeFCC/2_global.js
+++ NodeFCC/2_global.js
@@ -1,3 +1,11 @@
+// GLOBALS  - NO WINDOW !!!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+
 console.log(__dirname); //__dirname - path to current directory
 setInterval(()=>{
     console.log('hello world')
