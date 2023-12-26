63a5828260450f6729889ac63fb55a829bbc345b
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-26 14:17:44 +0530

    Add comments on 11_fs_async.js

--- NodeFCC/11_fs_async.js
+++ NodeFCC/11_fs_async.js
@@ -1,7 +1,10 @@
+//this is the asynchronous approach
 const { Console, log } = require('console');
 const {readFile, writeFile} = require('fs')
+//for asynchronous approach, we need to find a callback
 console.log('Start')
 readFile('./content/first.txt','utf8', (err,result) =>{
+    //(err,result) is the callback function
     if(err){
         console.log(err)
         return
@@ -11,7 +14,7 @@ readFile('./content/first.txt','utf8', (err,result) =>{
         if(err){
             console.log(err)
             return
-        }
+        }//inevitebly we'll reach a callback hell where we are nesting many callbacks
         const second = result;
         writeFile('./content/result-async.txt',
         `Here is the result : ${first}, ${second}`,(err,result)=>{
