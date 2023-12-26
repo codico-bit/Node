1cb3e3d9573c4ed1bbf1ba856f493f19f7fb0c53
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-26 13:39:14 +0530

    Add comments on how to use require in 3_modules.js

--- NodeFCC/3_module.js
+++ NodeFCC/3_module.js
@@ -1,10 +1,14 @@
+
 //CommonJS, every file is module (by default)
 //Modules - ENcapsulated Code (only share minimum)
 
 const names = require('./4_name')
 const sayHi = require('./5_utils')
 const data = require('./6_alt')
-require('./7_mind')
+require('./7_mind')//simply writing require and not
+//assigning to anything will just run that code
+//since we are calling the addValues() in ./7_mind
+//hence that function is called 
 
 //console.log(names);
 console.log(data);
