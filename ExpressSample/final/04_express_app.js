0e1190b042a7fb9dcaa834a6d18a14bdd5257bcd
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-26 23:45:38 +0530

    Add explainer comment

--- ExpressSample/final/04_express_app.js
+++ ExpressSample/final/04_express_app.js
@@ -5,6 +5,18 @@ const path = require('path');
 //now we have the same error we encountered while using only https 
 //however in express, we can just use app.use to fix it
 app.use(express.static('./public'))
+//app.use is for setting up the middleware
+//what does static mean? 
+/*
+it means that the server doesn't have to change the file
+since it is a static asset, we can just place it in a folder
+and we dump all the assets in there, in that folder
+it'll automatically set up all the paths etc for them
+example of static files: image file, javascript file
+Wait but how is javascript another static asset, 
+it makes it dynamic on the browser, but on the server 
+its just a static thing
+*/
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'));
     console.log('User hit the resource');
