73eaa8a7eff0a19ff42f5bb30e0cb22667429ee5
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-26 14:07:06 +0530

    Add comments on 9_path.js file

--- NodeFCC/9_path.js
+++ NodeFCC/9_path.js
@@ -1,12 +1,15 @@
-const path = require('path');
+const path = require('path');//built in module so we get it via require
 
-console.log(path.sep)
+console.log(path.sep)//the separator that is used in the system, example '/' is a separator
+
 
 const filePath = path.join('/content','subfolder///','test.txt');
-console.log(filePath);
+console.log(filePath);//you can add how many every ///, they format is
+//such that it is a valid output that can be directly used
 
 const base = path.basename(filePath)
-console.log(base);
+console.log(base); //the final file
 
 const absolute = path.resolve(__dirname,'content','subfolder','test.txt')
-console.log(absolute)
+console.log(absolute)//path.resolve returns the absolute path, it receives a sequence of paths
+//and then returns it in the formatted way
