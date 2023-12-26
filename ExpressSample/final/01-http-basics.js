9accedb1f1e52fe0948e8633be749295320b6a82
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-26 23:04:00 +0530

    Add if else for different url options

--- ExpressSample/final/01-http-basics.js
+++ ExpressSample/final/01-http-basics.js
@@ -1,12 +1,30 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/html'})
+    
+    const url = req.url
+    if(url==='/'){
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write('<h1>Home Page</h1>')
+        res.end()   
+    }else if(url==='/about'){
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write('<h1>About Page</h1>')
+        res.end() 
+    }else{
+        res.writeHead(404,{'content-type':'text/html'})
+        res.write('<h1>Page not found</h1>')
+        res.end() 
+    }
     //status code and header (key value pair)
-    res.write('<h1>Home Page</h1>')
+    //console.log(req.method);
+    //console.log(req.url); // outputs '/contact' if we're accessing contact, '/' if nothing
+    
+    
+    /*res.write('<h1>Home Page</h1>')
     res.end()
     //the above is same as
-    //res.end('<h1>Home Page</h1>')
+    //res.end('<h1>Home Page</h1>')*/
 })
 
 server.listen(5000);//listens on port 5000
