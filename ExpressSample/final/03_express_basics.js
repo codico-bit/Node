5d72edbb175fdf3e3a8cfeb6c20015ea7d8ef753
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-26 23:36:22 +0530

    Add different page options

--- ExpressSample/final/03_express_basics.js
+++ ExpressSample/final/03_express_basics.js
@@ -3,6 +3,22 @@ const app = express();
 
 //same as const app = require('express')();
 
+
+app.get('/',(req,res)=>{
+    res.status(200).send('Home Page');
+    console.log('User hit the resource');
+})
+//everytime user navigates to the root, we get the above output
+
+app.get('/about',(req,res)=>{
+    res.status(200).send('About Page');
+})
+
+//now for the remaining, we use .all, since it'll work for all of them
+app.all('*',(req,res)=>{
+    res.status(404).send('<h1>Resource Not Found</h1>');
+})
+
 app.listen(5000,()=>{
     console.log('The server is listening on 5000')
 })
@@ -19,3 +35,4 @@ app.listen(5000,()=>{
 // app.all - Works with all of them
 // app.use - Responsible for middleware, crucial part of express.js
 // app.listen - listens on a port
+
