22345cba60e15d8a87e7b5463bef1bd8df0c942e
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-26 19:44:48 +0530

    Write in the file using await

--- NodeFCC/1_async_patterns/2_await.js
+++ NodeFCC/1_async_patterns/2_await.js
@@ -1,7 +1,9 @@
-const {readFile} = require('fs');
+const {readFile, writeFile} = require('fs');
+
 const util = require('util');
 
 const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 /*
 const getText = (path) =>{
     return new Promise((resolve,reject)=>{
@@ -22,7 +24,8 @@ const start = async() =>{
         const first = await readFilePromise('./content/first.txt', 'utf8');
         //if we don't add the encoding 'utf8' then we'll get the buffer output
         const second = await readFilePromise('./content/second.txt', 'utf8');
-
+        await writeFilePromise('./content/result-mind-grenade.txt',`APPENDED THIS : ${first} ${second}` ) 
+        
         console.log(first, second);
     } catch(error) {
         console.log(error);
