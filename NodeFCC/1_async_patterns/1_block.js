0d1d3ea07a3c255e86e6a9edd439a84c0a52585f
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-26 17:58:23 +0530

    Add Block Async pattern

---
+++ NodeFCC/1_async_patterns/1_block.js
@@ -0,0 +1,23 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('Home Page')
+  }
+  else if (req.url === '/about') {
+    // blocking code
+    
+    res.end('About Page')
+    /*for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 1000; j++) {
+          console.log(`${i} ${j}`)
+        }
+    }*/
+  } else {
+    res.end('Error Page')
+  }
+})
+
+server.listen(5000, () => {
+  console.log('Server listening on port : 5000....')
+})
