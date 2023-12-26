916db1157c358b3111ca1d8946a5acb5f6b59680
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-26 21:53:15 +0530

    Add simple html response via res.write

--- ExpressSample/final/01-http-basics.js
+++ ExpressSample/final/01-http-basics.js
@@ -1,8 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log('User hit the server')
-    res.end('Home Page')
+    res.writeHead(200,{'content-type':'text/html'})
+    //status code and header (key value pair)
+    res.write('<h1>Home Page</h1>')
+    res.end()
+    //the above is same as
+    //res.end('<h1>Home Page</h1>')
 })
 
 server.listen(5000);//listens on port 5000
@@ -10,5 +14,6 @@ server.listen(5000);//listens on port 5000
 
 //everytime user hits the function, we have two functions, request and
 //response which we shorten and write as req and res by convention
+
 
 
