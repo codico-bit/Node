6922e1fc2cbf1ae425c5a985396075d4e8665858
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-26 21:06:29 +0530

    Change highWaterMark property and add guiding comments

--- NodeFCC/16_streams.js
+++ NodeFCC/16_streams.js
@@ -1,9 +1,16 @@
 
 const {createReadStream} = require('fs');
 
-const stream = createReadStream('./content/big.txt');
+const stream = createReadStream('./content/big.txt', {highWaterMark: 9000, encoding: 'utf8'});
 
 stream.on('data',(result)=>{
     console.log(result);
 })
+stream.on('error',(err)=>console.log(err))
 
+// default 64kb 
+// first argument is path, the highWaterMark controls the batch size
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
+// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
