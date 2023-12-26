74f00f82205f1761eec10febd3891d126654dff9
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-26 19:33:49 +0530

    Add method of using await to call asynchronous functions

---
+++ NodeFCC/1_async_patterns/2_await.js
@@ -0,0 +1,27 @@
+const {readFile} = require('fs');
+
+const getText = (path) =>{
+    return new Promise((resolve,reject)=>{
+        readFile(path, 'utf8', (err,data)=>{
+            if(err){
+                reject(err)
+            }else{
+                resolve(data)
+            }
+        })
+    })
+}
+
+//getText('./content/first.txt').then(result=>console.log(result)).catch(err=>console.log(err))
+
+const start = async() =>{
+    try{
+        const first = await getText('./content/first.txt');
+        const second = await getText('./content/second.txt');
+
+        console.log(first, second);
+    } catch(error) {
+        console.log(error);
+    }
+}
+start()
