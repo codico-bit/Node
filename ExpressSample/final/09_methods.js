eaffe8fa669ab4b8e983857af73ca4113043e101
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-27 21:49:44 +0530

    Add post method via postman

--- ExpressSample/final/09_methods.js
+++ ExpressSample/final/09_methods.js
@@ -47,7 +47,13 @@ app.post('/api/people', (req,res) =>{
     res.status(201).json({success:true, person:name})
 })
 
-
+app.post('/api/postman/people', (req,res) =>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success:false, msg:'please provide name value'})
+    }
+    res.status(201).json({success:true, data: [...people, name]})//we created another method via postman where we add it at the end of the data
+})
 
 app.post('/login', (req,res) =>{
     const {name} = req.body;
