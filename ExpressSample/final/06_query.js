4609d0b080c184f4188aafa3427c85d1b769c2c3
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-27 20:03:36 +0530

    Add query parameter and non matching productID

--- ExpressSample/final/06_query.js
+++ ExpressSample/final/06_query.js
@@ -19,16 +19,45 @@ app.get('/api/products',(req,res)=>{
 })
 //route parameter
 app.get('/api/products/:productID',(req,res)=>{
-   //console.log(req);
-   //console.log(req.params);
-   const {productID} = req.params;
-   const singleProduct = products.find((product) => product.id===Number(productID))
-   res.json(singleProduct);
-
-
+    //console.log(req);
+    //console.log(req.params);
+    const {productID} = req.params;
+    const singleProduct = products.find((product) => product.id===Number(productID))
     
+    if(!singleProduct){
+     return res.status(404).send('Product Does Not Exist')
+    }
+    
+     return res.json(singleProduct);
+     
+ })
+
+
+app.get('/api/products/:productID/reviews/:reviewID',(req,res)=>{
+    console.log(req.params);
+    res.send('Hello world');
 })
 
+app.get('/api/v1/query', (req, res) => {
+    // console.log(req.query)
+    const { search, limit } = req.query
+    let sortedProducts = [...products]
+  
+    if (search) {
+      sortedProducts = sortedProducts.filter((product) => {
+        return product.name.startsWith(search)
+      })
+    }
+    if (limit) {
+      sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if (sortedProducts.length < 1) {
+      // res.status(200).send('no products matched your search');
+      return res.status(200).json({ sucess: true, data: [] })
+    }
+    res.status(200).json(sortedProducts)
+  })
+  
 
 app.listen(5000,()=>{
     console.log('The server is listening on 5000')
