3ee93ec448c421f001a0221ea8c01a2266b693cd
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-27 20:17:45 +0530

    Add middleware via .use method

---
+++ ExpressSample/08_middleware_use.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const logger = require('./logger')
+// req => middleware => res
+
+
+app.use(logger)
+//app.use will invoke it for any route
+
+
+app.get('/', logger, (req,res)=>{
+    /*
+    //we write the below code as a function
+    //and then use it as a middleware
+    const method = req.method;
+    const url = req.url;
+    const time = new Date().getFullYear();
+    console.log(method,url,time);
+    */
+    res.send('Home');
+    // you see GET / 2023 in the terminal
+})
+
+app.get('/about',logger, (req,res)=>{
+    res.send('About');
+})
+
+
+app.listen(5000,()=>{
+    console.log('The server is listening on 5000')
+})
