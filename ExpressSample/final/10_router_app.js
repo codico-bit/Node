6c3421d210c37164d18ab3b63296dd131d2f8925
Swaraj Singh <72075925+swrjsingh@users.noreply.github.com>
2023-12-30 15:00:23 +0530

    Add comment

--- ExpressSample/final/10_router_app.js
+++ ExpressSample/final/10_router_app.js
@@ -1,26 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 //let {people} = require('./data')
-const people = require('./routes/people')
+const people = require("./routes/people");
 
-app.use(express.static('./methods-public'))
+app.use(express.static("./methods-public"));
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.json())
- 
-app.use('/api/people',people)
-//we're adding the base here so we don't include it in people.js 
+app.use(express.json());
 
-app.post('/login', (req,res) =>{
-    const {name} = req.body;
-    if(name){
-        return res.status(200).send(`Welcome ${name}`);
-    }
-    res.status(401).send('Please Provide Credentials')
-})
+app.use("/api/people", people);
+//we're adding the base here so we don't include it in people.js
 
-app.listen(5000,()=>{
-    console.log('The server is listening on 5000')
-})
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please Provide Credentials");
+});
+
+app.listen(5000, () => {
+  console.log("The server is listening on 5000");
+});
+
+//Test commit
